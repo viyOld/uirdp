43279a4a1d89df77d3a01f0ebef072e872cb7fc1
viy.2005@gmail.com <viy.2005@gmail.com>
2021-03-11 08:34:09 +0200

    20210310home

--- mainPage.go
+++ mainPage.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"strconv"
+
 	"github.com/gdamore/tcell"
 	"github.com/rivo/tview"
+)
+
+var (
+	headTable = []string{"№", " Group ", " Name ", " IP ", "Port", " Date ", " Comment "}
 )
 
 func getMainPage() *tview.Flex {
@@ -12,7 +18,24 @@ func getMainPage() *tview.Flex {
 	boxTop.SetTextAlign(tview.AlignCenter)
 	boxTop.SetBackgroundColor(tcell.Color(987))
 
-	boxMidlle := tview.NewBox().SetBorder(true).SetTitle("Midlle")
+	//boxMidlle := tview.NewBox().SetBorder(true).SetTitle("Midlle")
+	mainBodyTable := tview.NewTable().SetEvaluateAllRows(true).SetFixed(0, 0)
+	mainBodyTable.SetBorder(true).SetBorderPadding(2, 2, 3, 3)
+	mainBodyTable.SetSelectable(true, false)
+	//mainBodyTable.SetBorderPadding(2, 2, 3, 3)
+	for i := range headTable {
+		mainBodyTable.SetCellSimple(0, i, headTable[i])
+		mainBodyTable.GetCell(0, i).SetAlign(tview.AlignCenter) //.SetExpansion(1)
+	}
+	for j := range config.Servers {
+		mainBodyTable.SetCellSimple(j+1, 0, strconv.Itoa(j+1)) //SetMaxWidth(3)
+		mainBodyTable.SetCellSimple(j+1, 1, config.Servers[j].Group)
+		mainBodyTable.SetCellSimple(j+1, 2, config.Servers[j].Name)
+		mainBodyTable.SetCellSimple(j+1, 3, config.Servers[j].IP)
+		mainBodyTable.SetCellSimple(j+1, 4, strconv.Itoa(config.Servers[j].Port))
+		mainBodyTable.SetCellSimple(j+1, 5, config.Servers[j].Data)
+		mainBodyTable.SetCellSimple(j+1, 6, config.Servers[j].Comment)
+	}
 
 	txtMenu := tview.NewTable().SetEvaluateAllRows(true)
 	txtMenu.SetBorder(false)
@@ -23,11 +46,9 @@ func getMainPage() *tview.Flex {
 
 	mainPage := tview.NewFlex().SetDirection(tview.FlexRow).
 		AddItem(boxTop, 5, 3, false).
-		AddItem(boxMidlle, 0, 2, true).
+		AddItem(mainBodyTable, 0, 2, true).
 		AddItem(txtMenu, 1, 3, false)
 
 	return mainPage
 
 }
-
-//[]string{"№", " Group ", " Name ", " IP ", "Port", " Date ", " Comment "},
